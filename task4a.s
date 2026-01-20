744ba7dc0ad83ac464825b74279780182e0581aa
humna0809 <humna0809@gmail.com>
2026-01-20 13:42:40 +0500

    Update task4a.s

--- task4a.s
+++ task4a.s
@@ -1,26 +1,28 @@
-
 .text
 .globl main
 
-
 main:
+
+    # Initialize registers manually
+    li x10, 0x78786464
+    li x11, 0xA8A81919
 
     # Store x10 as unsigned integer at address 0x100.
     li x5, 0x100
-    sw x10 0(x5) # load into x10
+    sw x10, 0(x5)   # load into x10
    
     # Store x11 as unsigned integer at address 0x1F0.
-    li x6 , 0x1F0
-    sw x11 0(x6) #load into x11
+    li x6, 0x1F0
+    sw x11, 0(x6)   # load into x11
    
-    #Load an unsigned short integer (two bytes) from address 0x100 in x12.
-    lhu x12 0(x5) #oad into x12
+    # Load an unsigned short integer (two bytes) from address 0x100 in x12.
+    lhu x12, 0(x5)  # load into x12
 
-    #Load a short integer from address 0x1F0 in register x13.
-    lh x13 0(x6)  #load into x13
+    # Load a short integer from address 0x1F0 in register x13.
+    lh x13, 0(x6)   # load into x13
 
     # Load a singed character from address 0x1F0 in register x14.
-    lb x14 0(x6)#load into x14
+    lb x14, 0(x6)   # load into x14
 
 end:
     j end
